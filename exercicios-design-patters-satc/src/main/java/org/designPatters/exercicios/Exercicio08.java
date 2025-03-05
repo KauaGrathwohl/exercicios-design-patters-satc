f9df8d4f3386c516e1abc2d06564e3c34f04b7f6
kaua.110722 <kaua.grathwohl095@gmail.com>
2025-03-05 20:27:55 -0300

    Initial Commit

---
+++ exercicios-design-patters-satc/src/main/java/org/designPatters/exercicios/Exercicio08.java
@@ -0,0 +1,29 @@
+package org.designPatters.exercicios;
+
+public class Exercicio08 {
+
+    /*
+
+    # Exercicio 08
+
+    ## Sistema de Pagamentos
+    User Story:
+    Como um cliente de uma loja online, eu quero poder pagar minhas compras usando diferentes métodos de pagamento (cartão de crédito, PayPal, boleto), para que eu possa escolher a opção mais conveniente.
+
+    ### Requisitos:
+    Crie uma interface MetodoPagamento com o método:
+    - pagar(valor: number): void - Deve processar o pagamento.
+
+    Implemente a interface em três classes:
+    - CartaoCredito: Processa o pagamento por cartão de crédito (exiba no console: "Pagamento de R$[valor] realizado com cartão de crédito.").
+    - PayPal: Processa o pagamento via PayPal (exiba no console: "Pagamento de R$[valor] realizado via PayPal.").
+    - Boleto: Processa o pagamento por boleto (exiba no console: "Pagamento de R$[valor] realizado com boleto.").
+
+    Crie uma classe Compra com o atributo:
+    - valor (number)
+    - E um metodo realizarPagamento(metodo: MetodoPagamento) que usa a interface para processar o pagamento.
+
+    Teste o sistema criando uma compra e realizando pagamentos com diferentes métodos.
+
+    */
+}
